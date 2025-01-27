0a6189ca7df3db6f6173fcb7674eab7400caad72
Karungg <miftahfadilah71@gmail.com>
2025-01-27 13:52:08 +0700

    Add account observer

--- app/Models/Account.php
+++ app/Models/Account.php
@@ -2,11 +2,14 @@
 
 namespace App\Models;
 
+use App\Observers\AccountObserver;
+use Illuminate\Database\Eloquent\Attributes\ObservedBy;
 use Illuminate\Database\Eloquent\Concerns\HasUuids;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
+#[ObservedBy(AccountObserver::class)]
 class Account extends Model
 {
     use HasFactory, HasUuids;
---
+++ app/Observers/AccountObserver.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Observers;
+
+use App\Contracts\NotificationServiceInterface;
+use App\Models\Account;
+
+class AccountObserver
+{
+    public function __construct(protected NotificationServiceInterface $notificationService) {}
+    /**
+     * Handle the Account "created" event.
+     */
+    public function created(Account $account): void
+    {
+        $this->notificationService->sendSuccessNotification(
+            'Rekening berhasil ditambahkan.',
+            auth()->user()->name . ' menambahkan rekening atas nama ' . $account->customer->full_name,
+            $account,
+            'filament.admin.resources.accounts.index',
+            'account_number',
+            'adminManagement'
+        );
+    }
+
+    /**
+     * Handle the Account "updated" event.
+     */
+    public function updated(Account $account): void {}
+
+    /**
+     * Handle the Account "deleted" event.
+     */
+    public function deleted(Account $account): void
+    {
+        $this->notificationService->sendDeleteNotification(
+            'Rekening berhasil dihapus.',
+            auth()->user()->name . ' menghapus rekening atas nama ' . $account->customer->full_name,
+            'filament.admin.resources.accounts.index',
+            'adminManagement'
+        );
+    }
+}
