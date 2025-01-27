d5e5507925b06f691a4181bbdb0883b3c0074858
Karungg <miftahfadilah71@gmail.com>
2025-01-27 15:17:30 +0700

    Optimize product display resource

--- database/seeders/ProductDisplaySeeder.php
+++ database/seeders/ProductDisplaySeeder.php
@@ -3,7 +3,6 @@
 namespace Database\Seeders;
 
 use App\Models\ProductDisplay;
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
 class ProductDisplaySeeder extends Seeder
