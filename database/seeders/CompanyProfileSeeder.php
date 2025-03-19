c3a90d3ebe5a68439efc7be6508417a5dd852f46
Karungg <miftahfadilah71@gmail.com>
2025-03-19 16:19:12 +0700

    Remove unused namespace in seeders

--- database/seeders/CompanyProfileSeeder.php
+++ database/seeders/CompanyProfileSeeder.php
@@ -3,7 +3,6 @@
 namespace Database\Seeders;
 
 use App\Models\CompanyProfile;
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
 class CompanyProfileSeeder extends Seeder
