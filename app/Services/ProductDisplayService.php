593ba7eb9e2308fc570a72773a19e48f96c96ca3
Karungg <miftahfadilah71@gmail.com>
2025-03-21 07:36:18 +0700

    Add logging to product display service

--- app/Services/ProductDisplayService.php
+++ app/Services/ProductDisplayService.php
@@ -4,21 +4,39 @@ namespace App\Services;
 
 use App\Contracts\ProductDisplayServiceInterface;
 use App\Models\ProductDisplay;
+use Exception;
+use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Storage;
 
 class ProductDisplayService implements ProductDisplayServiceInterface
 {
     public function updateImage(ProductDisplay $productDisplay): void
     {
-        if ($productDisplay->isDirty('image') && $productDisplay->getOriginal('image') != null) {
-            Storage::disk('public')->delete($productDisplay->getOriginal('image'));
+        try {
+            if ($productDisplay->isDirty('image') && $productDisplay->getOriginal('image') != null) {
+                Storage::disk('public')->delete($productDisplay->getOriginal('image'));
+            }
+        } catch (Exception $e) {
+            Log::error("Failed to update image in product display resource", [
+                "product_display_id" => $productDisplay->id,
+                "message" => $e->getMessage(),
+            ]);
+            throw new Exception("Terjadi kesalahan saat memproses. Silahkan coba lagi nanti.");
         }
     }
 
     public function deleted(ProductDisplay $productDisplay): void
     {
-        if ($productDisplay->image) {
-            Storage::disk('public')->delete($productDisplay->image);
+        try {
+            if ($productDisplay->image) {
+                Storage::disk('public')->delete($productDisplay->image);
+            }
+        } catch (Exception $e) {
+            Log::error("Failed to delete image in product display resource", [
+                "product_display_id" => $productDisplay->id,
+                "message" => $e->getMessage(),
+            ]);
+            throw new Exception("Terjadi kesalahan saat memproses. Silahkan coba lagi nanti.");
         }
     }
 }
