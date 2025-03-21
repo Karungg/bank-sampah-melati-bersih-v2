dac1bd71890f1f1225b242122f2a8b86dd08b6c5
Karungg <miftahfadilah71@gmail.com>
2025-03-21 07:32:59 +0700

    Add logging to post service

--- app/Services/PostService.php
+++ app/Services/PostService.php
@@ -4,62 +4,80 @@ namespace App\Services;
 
 use App\Contracts\PostServiceInterface;
 use App\Models\Post;
+use Exception;
+use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Storage;
 
 class PostService implements PostServiceInterface
 {
     public function updateImage(Post $post): void
     {
-        if ($post->isDirty('images')) {
+        try {
+            if ($post->isDirty('images')) {
 
-            $originalFieldContents = $post->getOriginal('images');
-            $newFieldContents = $post->images;
+                $originalFieldContents = $post->getOriginal('images');
+                $newFieldContents = $post->images;
 
-            # We attempt to JSON decode the field. If it is an array, this is an indication we have ->multiple() activated
-            if (is_array($originalFieldContents)) {
-                $originalFieldContentsDecoded = $originalFieldContents;
-            } else {
-                $originalFieldContentsDecoded = json_decode($originalFieldContents);
-            }
+                # We attempt to JSON decode the field. If it is an array, this is an indication we have ->multiple() activated
+                if (is_array($originalFieldContents)) {
+                    $originalFieldContentsDecoded = $originalFieldContents;
+                } else {
+                    $originalFieldContentsDecoded = json_decode($originalFieldContents);
+                }
 
-            # Clean up empty entries in the resulting array
-            if (is_array($originalFieldContentsDecoded)) $originalFieldContentsDecoded = array_filter($originalFieldContentsDecoded);
+                # Clean up empty entries in the resulting array
+                if (is_array($originalFieldContentsDecoded)) $originalFieldContentsDecoded = array_filter($originalFieldContentsDecoded);
 
-            # Simple case: one file
-            if (!is_array($originalFieldContentsDecoded) or count($originalFieldContentsDecoded) == 0) {
-                Storage::disk('public')->delete($originalFieldContents);
-            }
+                # Simple case: one file
+                if (!is_array($originalFieldContentsDecoded) or count($originalFieldContentsDecoded) == 0) {
+                    Storage::disk('public')->delete($originalFieldContents);
+                }
 
-            # Complex case: multiple files
-            else {
-                foreach ($originalFieldContentsDecoded as $originalFile) {
-                    if (trim($originalFile) != null && !in_array($originalFile, $newFieldContents)) {
-                        Storage::disk('public')->delete($originalFile);
+                # Complex case: multiple files
+                else {
+                    foreach ($originalFieldContentsDecoded as $originalFile) {
+                        if (trim($originalFile) != null && !in_array($originalFile, $newFieldContents)) {
+                            Storage::disk('public')->delete($originalFile);
+                        }
                     }
                 }
             }
+        } catch (Exception $e) {
+            Log::error("Failed to update image in post resource", [
+                "post_id" => $post->id,
+                "message" => $e->getMessage(),
+            ]);
+            throw new Exception("Terjadi kesalahan saat memproses. Silahkan coba lagi nanti.");
         }
     }
 
     public function deleted(Post $post): void
     {
-        if (! is_null($post->images)) {
+        try {
+            if (! is_null($post->images)) {
 
-            # We attempt to JSON decode the field. If it is an array, there are multiple files
-            $fieldContentsDecoded = json_decode($post->images);
+                # We attempt to JSON decode the field. If it is an array, there are multiple files
+                $fieldContentsDecoded = json_decode($post->images);
 
-            # Simple case: one file
-            if (!is_array($fieldContentsDecoded)) {
-                Storage::disk('public')->delete($post->images);
-            }
+                # Simple case: one file
+                if (!is_array($fieldContentsDecoded)) {
+                    Storage::disk('public')->delete($post->images);
+                }
 
-            # Complex case: multiple files
-            else {
+                # Complex case: multiple files
+                else {
 
-                foreach ($fieldContentsDecoded as $file) {
-                    Storage::disk('public')->delete($file);
+                    foreach ($fieldContentsDecoded as $file) {
+                        Storage::disk('public')->delete($file);
+                    }
                 }
             }
+        } catch (Exception $e) {
+            Log::error("Failed to delete image in post resource", [
+                "post_id" => $post->id,
+                "message" => $e->getMessage(),
+            ]);
+            throw new Exception("Terjadi kesalahan saat memproses. Silahkan coba lagi nanti.");
         }
     }
 }
