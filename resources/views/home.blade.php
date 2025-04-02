a06dede642982be122e899ecc1833176d740d82e
Karungg <miftahfadilah71@gmail.com>
2025-04-02 08:56:37 +0700

    Modify home

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -109,8 +109,8 @@
             </div>
             <div class="grid grid-cols-2 md:grid-cols-3 gap-8 sm:gap-16">
                 <div>
-                    <div class="card h-36 sm:h-32 lg:h-36 xl:h-auto" :class="darkMode ? 'steps-dark' : 'steps'">
-                        <div class="card-body">
+                    <div class="card" :class="darkMode ? 'steps-dark' : 'steps'">
+                        <div class="card-body p-2">
                             <div class="flex justify-center">
                                 <svg class="size-6 md:size-8 lg:size-10" xmlns="http://www.w3.org/2000/svg"
                                     fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
@@ -118,15 +118,15 @@
                                         d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0" />
                                 </svg>
                             </div>
-                            <p class="text-sm sm:text-center md:text-base lg:text-lg">1. Nasabah memilah
+                            <p class="text-xs sm:text-center md:text-base lg:text-lg">1. Nasabah memilah
                                 sampah di rumah
                             </p>
                         </div>
                     </div>
                 </div>
                 <div>
-                    <div class="card h-36 sm:h-32 lg:h-36 xl:h-auto" :class="darkMode ? 'steps-dark' : 'steps'">
-                        <div class="card-body">
+                    <div class="card" :class="darkMode ? 'steps-dark' : 'steps'">
+                        <div class="card-body p-2">
                             <div class="flex justify-center">
                                 <svg class="size-6 md:size-8 lg:size-10" xmlns="http://www.w3.org/2000/svg"
                                     fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
@@ -136,15 +136,15 @@
                                         d="M19.5 10.5c0 7.142-7.5 11.25-7.5 11.25S4.5 17.642 4.5 10.5a7.5 7.5 0 1 1 15 0Z" />
                                 </svg>
                             </div>
-                            <p class="text-sm sm:text-center md:text-base lg:text-lg">2. Datang ke
+                            <p class="text-xs sm:text-center md:text-base lg:text-lg">2. Datang ke
                                 lokasi bank sampah
                             </p>
                         </div>
                     </div>
                 </div>
                 <div>
-                    <div class="card h-36 sm:h-32 lg:h-36 xl:h-auto" :class="darkMode ? 'steps-dark' : 'steps'">
-                        <div class="card-body">
+                    <div class="card" :class="darkMode ? 'steps-dark' : 'steps'">
+                        <div class="card-body p-2">
                             <div class="flex justify-center">
                                 <svg class="size-6 md:size-8 lg:size-10 xmlns=" http://www.w3.org/2000/svg"
                                     fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
@@ -152,15 +152,15 @@
                                         d="M18 7.5v3m0 0v3m0-3h3m-3 0h-3m-2.25-4.125a3.375 3.375 0 1 1-6.75 0 3.375 3.375 0 0 1 6.75 0ZM3 19.235v-.11a6.375 6.375 0 0 1 12.75 0v.109A12.318 12.318 0 0 1 9.374 21c-2.331 0-4.512-.645-6.374-1.766Z" />
                                 </svg>
                             </div>
-                            <p class="text-sm sm:text-center md:text-base lg:text-lg">3. Mendaftar
+                            <p class="text-xs sm:text-center md:text-base lg:text-lg">3. Mendaftar
                                 nasabah baru
                             </p>
                         </div>
                     </div>
                 </div>
                 <div>
-                    <div class="card h-36 sm:h-32 lg:h-36 xl:h-auto" :class="darkMode ? 'steps-dark' : 'steps'">
-                        <div class="card-body">
+                    <div class="card" :class="darkMode ? 'steps-dark' : 'steps'">
+                        <div class="card-body p-2">
                             <div class="flex justify-center">
                                 <svg class="size-6 md:size-8 lg:size-10 xmlns=" http://www.w3.org/2000/svg"
                                     fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
@@ -168,15 +168,14 @@
                                         d="M12 3v17.25m0 0c-1.472 0-2.882.265-4.185.75M12 20.25c1.472 0 2.882.265 4.185.75M18.75 4.97A48.416 48.416 0 0 0 12 4.5c-2.291 0-4.545.16-6.75.47m13.5 0c1.01.143 2.01.317 3 .52m-3-.52 2.62 10.726c.122.499-.106 1.028-.589 1.202a5.988 5.988 0 0 1-2.031.352 5.988 5.988 0 0 1-2.031-.352c-.483-.174-.711-.703-.59-1.202L18.75 4.971Zm-16.5.52c.99-.203 1.99-.377 3-.52m0 0 2.62 10.726c.122.499-.106 1.028-.589 1.202a5.989 5.989 0 0 1-2.031.352 5.989 5.989 0 0 1-2.031-.352c-.483-.174-.711-.703-.59-1.202L5.25 4.971Z" />
                                 </svg>
                             </div>
-                            <p class="text-sm sm:text-center md:text-base lg:text-lg">4. Menimbang
-                                sampah
+                            <p class="text-xs sm:text-center md:text-base lg:text-lg">4. Menimbang sampah
                             </p>
                         </div>
                     </div>
                 </div>
                 <div>
-                    <div class="card h-36 sm:h-32 lg:h-36 xl:h-auto" :class="darkMode ? 'steps-dark' : 'steps'">
-                        <div class="card-body">
+                    <div class="card" :class="darkMode ? 'steps-dark' : 'steps'">
+                        <div class="card-body p-2">
                             <div class="flex justify-center">
                                 <svg class="size-6 md:size-8 lg:size-10 xmlns=" http://www.w3.org/2000/svg"
                                     fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
@@ -184,15 +183,15 @@
                                         d="M10.125 2.25h-4.5c-.621 0-1.125.504-1.125 1.125v17.25c0 .621.504 1.125 1.125 1.125h12.75c.621 0 1.125-.504 1.125-1.125v-9M10.125 2.25h.375a9 9 0 0 1 9 9v.375M10.125 2.25A3.375 3.375 0 0 1 13.5 5.625v1.5c0 .621.504 1.125 1.125 1.125h1.5a3.375 3.375 0 0 1 3.375 3.375M9 15l2.25 2.25L15 12" />
                                 </svg>
                             </div>
-                            <p class="text-sm sm:text-center md:text-base lg:text-lg">5. Menerima bukti
+                            <p class="text-xs sm:text-center md:text-base lg:text-lg">5. Menerima bukti
                                 timbang
                             </p>
                         </div>
                     </div>
                 </div>
                 <div>
-                    <div class="card h-36 sm:h-32 lg:h-36 xl:h-auto" :class="darkMode ? 'steps-dark' : 'steps'">
-                        <div class="card-body">
+                    <div class="card" :class="darkMode ? 'steps-dark' : 'steps'">
+                        <div class="card-body p-2">
                             <div class="flex justify-center">
                                 <svg class="size-6 md:size-8 lg:size-10 xmlns=" http://www.w3.org/2000/svg"
                                     fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
@@ -200,7 +199,7 @@
                                         d="M8.25 9V5.25A2.25 2.25 0 0 1 10.5 3h6a2.25 2.25 0 0 1 2.25 2.25v13.5A2.25 2.25 0 0 1 16.5 21h-6a2.25 2.25 0 0 1-2.25-2.25V15M12 9l3 3m0 0-3 3m3-3H2.25" />
                                 </svg>
                             </div>
-                            <p class="text-sm sm:text-center md:text-base lg:text-lg">6. Login untuk
+                            <p class="text-xs sm:text-center md:text-base lg:text-lg">6. Login untuk
                                 mengecek status
                             </p>
                         </div>
@@ -226,7 +225,7 @@
                                 <img class="w-full h-24 sm:h-36 xl:h-64 object-cover rounded-md"
                                     src="{{ asset('assets/images/posts/post-1.jpg') }}" alt="Gambar">
                             </figure>
-                            <div class="card-body p-0 mt-2">
+                            <div class="card-body p-2 mt-2">
                                 <h2 class="card-title text-xs lg:text-base">Kegiatan Transfer Knowledge
                                     Green Economy dan
                                     Workshop Pengolahan
